e37ebdec3c5b547fe7f225645b48ba09c1e7617f
Leon Trestman <tleonleont@gmail.com>
2024-09-17 04:22:42 +0300

    Refactor nameing in data

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,4 +1,4 @@
-import { PERSONAL_DATA } from "../data/Data";
+import { BIO_DATA } from "../data/Data";
 import myPic from "../assets/myPic.jpg";
 
 const Bio = () => {
@@ -18,9 +18,9 @@ const Bio = () => {
       </div>
       <div className="flex w-full flex-col gap-2 text-white lg:justify-center">
         <h1 className="text-center text-3xl sm:text-4xl md:text-5xl">
-          {PERSONAL_DATA.name}
+          {BIO_DATA.name}
         </h1>
-        <p className="text-center text-sm">{PERSONAL_DATA.discription}</p>
+        <p className="text-center text-sm">{BIO_DATA.discription}</p>
       </div>
     </section>
   );
--- src/data/Data.tsx
+++ src/data/Data.tsx
@@ -4,7 +4,7 @@ import { SiTypescript } from "react-icons/si";
 
 export const NAVIGATION_LINKS = ["Bio", "Skills", "Projects"];
 
-export const PERSONAL_DATA = {
+export const BIO_DATA = {
   name: "Leon Trestman",
   discription:
     "teLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat, ex vel convallis posuere, dui libero molestie diam, non aliquam arcu augue non eros. Fusce a pulvinar velit, quis varius dolor. Praesent dolor urna, bibendum ut lacus nec, sodales pellentesque sapien. Sed risus neque, pulvinar id venenatis et, congue id nisi. Suspendisse faucibus finibus risus, sit amet blandit magna elementum quis. Sed id mauris a nibh hendrerit ullamcorper. Vestibulum volutpat sem nec nisi cursus, sed mollis massa pulvinar. Duis sodales nisl sed imperdiet fermentum. Maecenas posuere ipsum ut convallis accumsan. Aenean quis orci sit amet nisl aliquet placerat eget consectetur odio. Nulla in sagittis purus. Sed sed egestas dui, sit amet vulputate ante. Cras ac feugiat quam. Sed eget sem pellentesque, tincidunt nulla at, ultrices nisi. Nulla vehicula sollicitudin dapibus. Phasellus sit amet elit justo.st",
