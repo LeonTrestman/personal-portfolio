2a51546488b4a0fba09e381b20ce4903781ef38c
Leon Trestman <tleonleont@gmail.com>
2024-09-23 21:03:29 +0300

    reworked bio flexboxes ,started workin on animited text

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,28 +1,61 @@
 import { BIO_DATA } from "../data/Data";
 import myPic from "../assets/myPic.jpg";
+import { motion, AnimatePresence, useInView } from "framer-motion";
+import { useRef } from "react";
 
 const Bio = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false, amount: 0 });
+
   return (
     <section
-      className="full-section mx-auto flex max-w-3xl flex-col gap-8 p-4 md:flex-row-reverse md:items-center md:gap-2"
+      // className="full-section mx-auto flex max-w-3xl flex-col gap-8 p-4 md:flex-row-reverse md:items-center md:gap-2"
+      className="full-section p-8"
       id="Bio"
     >
-      <div className="flex w-full justify-center">
+      <motion.div
+        ref={ref}
+        className="m-auto flex h-full flex-col items-center justify-center gap-12 md:flex-row-reverse"
+      >
+        {/* Pic Part */}
+
         <img
-          height={300}
-          width={300}
           src={myPic}
-          className="rounded-full shadow-lg shadow-fuchsia-700 md:w-full"
+          className="size-fit max-w-xs rounded-full shadow-lg shadow-fuchsia-700"
           alt="LeonTrestmanPic"
         />
-      </div>
-      <div className="flex w-full flex-col gap-2 text-white lg:justify-center">
-        <h1 className="text-center text-3xl sm:text-4xl md:text-5xl">
-          {BIO_DATA.name}
-        </h1>
-        <p className="text-center text-sm">{BIO_DATA.discription}</p>
-      </div>
+
+        {/* text part */}
+        <div className="flex flex-col gap-2 text-center text-white">
+          <h2>Hey, my name is:</h2>
+          <h1 className="text-3xl sm:text-4xl md:text-5xl">{BIO_DATA.name}</h1>
+          <p>
+            <AnimatedTitlesProps isVisible={isInView} />
+          </p>
+          <p className="text-sm">{BIO_DATA.discription}</p>
+        </div>
+      </motion.div>
     </section>
+  );
+};
+
+type animatedTitlesProps = {
+  isVisible: boolean;
+};
+
+const AnimatedTitlesProps = ({ isVisible }: animatedTitlesProps) => {
+  return (
+    <AnimatePresence>
+      {isVisible && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <div>test</div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
--- src/data/Data.tsx
+++ src/data/Data.tsx
@@ -18,6 +18,7 @@ export type Project = {
 
 export type BioData = {
   name: string;
+  titles: string[];
   discription: string;
 };
 
@@ -25,8 +26,13 @@ export const NAVIGATION_LINKS = ["Bio", "Skills", "Projects", "Contact"];
 
 export const BIO_DATA: BioData = {
   name: "Leon Trestman",
+  titles: [
+    "Software engineer",
+    "Computer science graduate",
+    "Full stack developer",
+  ],
   discription:
-    "teLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat, ex vel convallis posuere, dui libero molestie diam, non aliquam arcu augue non eros. Fusce a pulvinar velit, quis varius dolor. Praesent dolor urna, bibendum ut lacus nec, sodales pellentesque sapien. Sed risus neque, pulvinar id venenatis et, congue id nisi. Suspendisse faucibus finibus risus, sit amet blandit magna elementum quis. Sed id mauris a nibh hendrerit ullamcorper. Vestibulum volutpat sem nec nisi cursus, sed mollis massa pulvinar. Duis sodales nisl sed imperdiet fermentum. Maecenas posuere ipsum ut convallis accumsan. Aenean quis orci sit amet nisl aliquet placerat eget consectetur odio. Nulla in sagittis purus. Sed sed egestas dui, sit amet vulputate ante. Cras ac feugiat quam. Sed eget sem pellentesque, tincidunt nulla at, ultrices nisi. Nulla vehicula sollicitudin dapibus. Phasellus sit amet elit justo.st",
+    "teLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat, ex vel convallis posuere ",
 };
 
 export const SKILLS_DATA = [
